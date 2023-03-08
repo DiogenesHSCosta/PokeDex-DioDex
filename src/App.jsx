f9acdad7a06c5e20d5b8665fe3f96e2ff4fb2327
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-03-08 16:27:58 -0300

    Estrutura do projeto consertada

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { Caixa } from './components/Caixa/Caixa.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid-container">
+
+        <section className='container lista'>
+        <h1>POKEMONS</h1>
+
+          <ul>
+            <li>item 1</li>
+          </ul>
+
+        <div className='caixa-container'>
+          <Caixa texto="ANTERIOR" />
+          <Caixa texto="PROXIMO" />
+        </div>
+        
+        </section>
+        
+        <section className='container descricao'>
+          <p>descricao</p>
+
+          <figure className='descricao-texto'>
+
+          </figure>
+
+          <div className='descricao-texto'>
+          
+          </div>
+        </section>
+          
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </div>
   )
 }
---
+++ src/components/Caixa/Caixa.jsx
@@ -0,0 +1,11 @@
+import "./Caixa.css"
+
+export function Caixa(props){
+
+    return(
+    <div className="caixa">
+        <p>{props.texto}</p>
+    </div>
+    )
+    
+}
