21c458d35c92b915648c34a49450835cfdea7e15
Diogenes Costa <diogenes.henriquecosta@outlook.com>
2023-03-14 09:40:54 -0300

    Correção de renderização

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,37 @@ import { Caixa } from './components/Caixa/Caixa.jsx'
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [descricao, setDescricao] = useState()
-  const dados=[]
-  let alvo;
-
-  // useEffect(() => {
-  //   getApi();
-  // }, []);
+ 
+  useEffect(() => {
+    getApi();
+  }, []);
 
   async function getApi(){
-    for(let i =1; i <= 10; i++){
+    const dados=[]
+
+    for(let i =1; i <= 20; i++){
       dados.push( await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`))
       console.log(`For rodou ${i} vez(es)`)
     }
+
     setPokemon(dados)
   }
+  
+
 function teste(){
-    console.log(descricao)
+    console.log(pokemon)
     
   }
 
+
   function showDesc(e){
-    console.log(e.target.innerText)
-    pokemon.map((element) =>{
-      if(element.data.name == e.target.innerText){
-        setDescricao(element.data)
-      }
-    })
+
+      pokemon.map((element) =>{
+        if(element.data.name == e.target.innerText){
+          setDescricao(element.data)
+        }
+      })
+
   }
 
   
@@ -43,10 +48,11 @@ function teste(){
         <h1>POKEMONS</h1>
 
           <ul>
-            
             {pokemon.map((elemento) =>(
               <li key={elemento.data.id}>
-                <p onClick={(e)=> showDesc(e)}>{elemento.data.name}</p>
+                <p onClick={(e)=> showDesc(e)}>
+                  {elemento.data.name}
+                </p>
               </li>
             ))}
           </ul>
@@ -59,29 +65,43 @@ function teste(){
         </section>
         
         <section className='container descricao'>
-          <p>descricao</p>
-
-          <div className='figure'>
+          
+          {descricao == null ? 
+          (<p>bom dia</p>)
+          :(
+          <div className='descricao-container'>
+            <p>descricao</p>
+            <div className='figure'>
               <figure>
                 <img src={descricao.sprites.front_default} alt="" />
               </figure>
-          </div>
+            </div>
+          
+            <div className="descricao-tipo">
+              {descricao.types.length == 1 ? (
+              <Caixa texto={descricao.types[0].type.name}/>
+              ):
+              (<div>
+                <Caixa texto={descricao.types[0].type.name}/>
+                <Caixa texto={descricao.types[1].type.name}/>
+              </div>)}
+            </div>
 
-          {/* <div className="descricao-tipo">
-            <Caixa texto={descricao.types[0].type.name}/>
-            <Caixa texto={descricao.types[1].type.name}/>
-          </div> */}
-          <div className='descricao-status'>
-              <ul>
-                <li><p>HP: {descricao.stats[0].base_stat}</p></li>
-                <li><p>ATAQUE: {descricao.stats[1].base_stat}</p></li>
-                <li><p>DEFESA: {descricao.stats[2].base_stat}</p></li>
-                <li><p>ATAQUE ESPECIAL: {descricao.stats[3].base_stat}</p></li>
-                <li><p>DEFESA ESPECIAL: {descricao.stats[4].base_stat}</p></li>
-                <li><p>SPEED: {descricao.stats[5].base_stat}</p></li>
-              </ul>
-            
+
+            <div className='descricao-status'>
+                <ul>
+                  <li><p>HP: {descricao.stats[0].base_stat}</p></li>
+                  <li><p>ATAQUE: {descricao.stats[1].base_stat}</p></li>
+                  <li><p>DEFESA: {descricao.stats[2].base_stat}</p></li>
+                  <li><p>ATAQUE ESPECIAL: {descricao.stats[3].base_stat}</p></li>
+                  <li><p>DEFESA ESPECIAL: {descricao.stats[4].base_stat}</p></li>
+                  <li><p>SPEED: {descricao.stats[5].base_stat}</p></li>
+                </ul>
+              
+            </div>
           </div>
+          )}
+          
         </section>
           
       </div>
